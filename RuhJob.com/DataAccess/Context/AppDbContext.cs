8699ca8b2dda56100e3a235a6f838617813b88cd
Ariz <Ariz@DESKTOP-HI3RPRU>
2025-01-20 21:50:06 +0400

    update

---
+++ RuhJob.com/DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+using RuhJob.com.DataAccess.Entity;
+
+namespace RuhJob.com.DataAccess.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+    }
+}
