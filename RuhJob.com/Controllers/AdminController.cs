868c09c591a1ee06068e443631df6861e8069c00
Ariz <Ariz@DESKTOP-HI3RPRU>
2025-02-19 22:07:11 +0400

    update

--- RuhJob.com/Controllers/AdminController.cs
+++ RuhJob.com/Controllers/AdminController.cs
@@ -89,5 +89,40 @@ namespace RuhJob.com.Controllers
             return RedirectToAction("Contact");
         }
 
+
+        public IActionResult Jobs() 
+        {
+            var values = _db.Jobs.ToList();
+            return View(values);
+        }
+
+        public IActionResult JobDelete(int id)
+        {
+
+            var ent = _db.Jobs.Find(id);
+            _db.Remove(ent);
+            _db.SaveChanges();
+            return RedirectToAction("Jobs");
+        }
+
+
+        [HttpGet]
+        public IActionResult JobUpdate(int id)
+        {
+            ViewBag.list = _db.Categories.ToList();
+            var values = _db.Jobs.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult JobUpdate(Job p)
+        {
+            ViewBag.list = _db.Categories.ToList();
+            _db.Update(p);
+            _db.SaveChanges();
+            return RedirectToAction("Jobs");
+        }
+
+
     }
 }
